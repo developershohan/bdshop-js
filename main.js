6b7cb9487bc408623e6f30c3ebabffc3384c1326
Shohan <dev.shohanur@gmail.com>
2024-08-13 12:37:38 +0600

    fixed js loading

--- main.js
+++ main.js
@@ -235,77 +235,82 @@ document.addEventListener("DOMContentLoaded", () => {
 // });
 
 // get product from api
-try {
-  const res = await axios.get("https://backend-captain-api.vercel.app/api/v1/product/");
-  const products = res.data;
-  console.log(products);
 
-
-  let content = "";
-
-  if (products.length > 0) {
-    products.forEach((product, index) => {
-      content += `
-        <div
-          class="card lg:w-[31%] md:w-[47%] w-full flex flex-col justify-center items-center text-center gap-5" id = card${product._id}>
-          <div class="product_img w-full relative overflow-hidden">
-            <a href="./singlePage.html?id=${product._id}">
-              <img
-                class="w-full bg-black h-[300px] object-cover"
-                src="${product.imageUrl}"
-                alt="" />
-            </a>
-            <div
-              class="add_to_cart_icon absolute bg-red-600 flex justify-center items-center p-5 bottom-0 right-0">
-              <svg role="img" class="h-6 w-6 fill-white">
-                <use xlink:href="assets/img/sprite.svg#shopping-basket"></use>
-              </svg>
+const getProduct  = async ()=>{
+  try {
+    const res = await axios.get("https://backend-captain-api.vercel.app/api/v1/product/");
+    const products = res.data;
+  
+    console.log(products);
+  
+  
+    let content = "";
+  
+    if (products.length > 0) {
+      products.forEach((product, index) => {
+        content += `
+          <div
+            class="card lg:w-[31%] md:w-[47%] w-full flex flex-col justify-center items-center text-center gap-5" id = card${product._id}>
+            <div class="product_img w-full relative overflow-hidden">
+              <a href="./singlePage.html?id=${product._id}">
+                <img
+                  class="w-full bg-black h-[300px] object-cover"
+                  src="${product.imageUrl}"
+                  alt="" />
+              </a>
+              <div
+                class="add_to_cart_icon absolute bg-red-600 flex justify-center items-center p-5 bottom-0 right-0">
+                <svg role="img" class="h-6 w-6 fill-white">
+                  <use xlink:href="assets/img/sprite.svg#shopping-basket"></use>
+                </svg>
+              </div>
+            </div>
+            <div class="product_details">
+              <p class="product_category">${product.category}</p>
+              <a
+                href="./cart.html"
+                class="hover:text-red-500 text-[18px] font-bold">
+                ${product.name}</a
+              >
+              <p class="price font-bold text-[16px]">
+                $<span class="sale_price text-red-500 pr-1">${product.baseprice}</span>
+                <span class="regular_price line-through text-gray-400">$80</span>
+              </p>
             </div>
           </div>
-          <div class="product_details">
-            <p class="product_category">${product.category}</p>
-            <a
-              href="./cart.html"
-              class="hover:text-red-500 text-[18px] font-bold">
-              ${product.name}</a
-            >
-            <p class="price font-bold text-[16px]">
-              $<span class="sale_price text-red-500 pr-1">${product.baseprice}</span>
-              <span class="regular_price line-through text-gray-400">$80</span>
-            </p>
-          </div>
-        </div>
-      
-      `;
-    });
-    const product_container = document.querySelector(
-      ".product_section .container"
-    );
-    product_container.innerHTML = content;
-  }
-
-  const uniqueCategories = new Set();
-  products.forEach((product) => {
-    uniqueCategories.add(product.category);
-  });
-
-
-  uniqueCategories.forEach((category) => {
-    content += `
-            <option value="${category}">${category}</option>
-
-`;
-  });
-  const product_category = document.querySelector("#category_filter");
-  product_category.innerHTML = content;
-
-
-
+        
+        `;
+      });
+      const product_container = document.querySelector(
+        ".product_section .container"
+      );
+      product_container.innerHTML = content;
+    }
   
-} catch (error) {
-  console.error(error);
+    const uniqueCategories = new Set();
+    products.forEach((product) => {
+      uniqueCategories.add(product.category);
+    });
+  
+  
+    uniqueCategories.forEach((category) => {
+      content += `
+              <option value="${category}">${category}</option>
+  
+  `;
+    });
+    const product_category = document.querySelector("#category_filter");
+    product_category.innerHTML = content;
+  
+  
+  
+    
+  } catch (error) {
+    console.error(error);
+  }
+  
 }
-
+getProduct()
 // single product details
 
 const getSingleproduct = async () => {
